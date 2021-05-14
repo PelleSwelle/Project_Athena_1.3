99b84babf5eb06067b86720cbf35a4d7a7975a94
NichlasLaursen <72730726+NichlasLaursen@users.noreply.github.com>
2021-05-14 14:32:20 +0200

    wip, bare for at sørge for at koden er der

--- app/src/main/java/com/example/projectathena13/ui/achievements/AchievementsFragment.java
+++ app/src/main/java/com/example/projectathena13/ui/achievements/AchievementsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +24,22 @@ public class AchievementsFragment extends Fragment
         achievementsViewModel = new ViewModelProvider(this).get(AchievementsViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_achievements, container, false);
+
+        Button achpop1 = getView().findViewById(R.id.button4);
+        LinearLayout ach1 = getView().findViewById(R.id.ach1);
+        achpop1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ach1.getVisibility() == View.GONE)
+                {
+                    ach1.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    ach1.setVisibility(View.GONE);
+                }
+            }
+        });
 
 //        achievementsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>()
 //        {
