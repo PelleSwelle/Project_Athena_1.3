7e7b8cce0c6fe72c31dd8cd4f7ef4d6ef1395438
Nichlaur <nlaurs20@student.aau.dk>
2021-05-17 15:47:19 +0200

    onClick til forst lesson achievement er klar, men har kun ph billeder atm

--- app/src/main/java/com/example/projectathena13/ui/achievements/AchievementsFragment.java
+++ app/src/main/java/com/example/projectathena13/ui/achievements/AchievementsFragment.java
@@ -5,12 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.projectathena13.R;
 
@@ -25,18 +25,24 @@ public class AchievementsFragment extends Fragment
 
         View root = inflater.inflate(R.layout.fragment_achievements, container, false);
 
-        Button achpop1 = getView().findViewById(R.id.button4);
-        LinearLayout ach1 = getView().findViewById(R.id.ach1);
+        ImageButton achpop1 = root.findViewById(R.id.imageButton);
+        ImageButton achclose1 = root.findViewById(R.id.imageButton2);
+        ConstraintLayout achpopL = root.findViewById(R.id.ach1);
         achpop1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ach1.getVisibility() == View.GONE)
+                if (achpopL.getVisibility() == View.GONE)
                 {
-                    ach1.setVisibility(View.VISIBLE);
+                    achpopL.setVisibility(View.VISIBLE);
                 }
-                else
+            }
+        });
+        achclose1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (achpopL.getVisibility() == View.VISIBLE)
                 {
-                    ach1.setVisibility(View.GONE);
+                    achpopL.setVisibility(View.GONE);
                 }
             }
         });
